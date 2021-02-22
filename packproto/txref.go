40832fcdc27bff6ca33bc5cab21a3a2347219f2f
Christian Roggia <christian.roggia@gmail.com>
2021-02-22 23:29:03 +0100

    Fix malformed properly and add documentation

--- packproto/txref.go
+++ packproto/txref.go
@@ -16,8 +16,16 @@ type txRef struct {
 
 // Parses old hash, new hash, and ref from a line in that order
 func newTxRefFromBytes(line []byte) (rt txRef, err error) {
-	arr := strings.Split(string(line), " ")
-	if len(arr) < 3 {
+	// ! https://github.com/git/git/blob/v2.30.1/Documentation/technical/http-protocol.txt
+	// ! We expect the format to be something like
+	// ! "....0a53e9ddeaddad63ad106860237bbf53411d11a7 441b40d833fdfa93eb2908e52742248faf0ee993 refs/heads/master\0 report-status"
+	// ! but due to https://github.com/go-git/go-git/blob/v5.2.0/plumbing/protocol/packp/updreq_encode.go#L57
+	// ! the encoding is broken and there is no space between the "\x00" byte and the list of capabilities.
+	// ! See also https://github.com/go-git/go-git/blob/v5.2.0/plumbing/protocol/packp/updreq_encode_test.go#L112
+	tokens := strings.Split(string(line), "\x00")
+
+	arr := strings.Split(strings.TrimSpace(tokens[0]), " ")
+	if len(arr) != 3 {
 		err = errors.New("invalid line: " + string(line))
 		return
 	}
@@ -25,7 +33,7 @@ func newTxRefFromBytes(line []byte) (rt txRef, err error) {
 	rt = txRef{
 		oldHash: plumbing.NewHash(arr[0]),
 		newHash: plumbing.NewHash(arr[1]),
-		ref:     strings.Split(arr[2], string([]byte{0}))[0],
+		ref:     arr[2],
 	}
 
 	return
